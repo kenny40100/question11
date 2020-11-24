5c51eb362341d23af16f46334c638f654f2ebf15
kenny40100 <71364976+kenny40100@users.noreply.github.com>
2020-11-24 14:34:11 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("-->")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString(":D")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("<--")
+})
